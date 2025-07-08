aa8585ac84642397b5fdf02a8525e3cebbead546
Prince-861 <pk868317@example.com>
2025-07-08 23:23:04 +0530

    "ThreeSum optimal solution added"

--- src/com/arrays/ThreeSum.java
+++ src/com/arrays/ThreeSum.java
@@ -41,20 +41,47 @@ public class ThreeSum {
 //    }
 
     //    Method-2 ---> T.C===> O(n^2) S.C ==> O(n^2)
+//    public static List<List<Integer>> threeSum(int[] nums){
+//        Set<List<Integer>> set = new HashSet<>();
+//        for(int i=0;i<nums.length;i++){
+//            Set<Integer>hashSet = new HashSet<>();
+//            for(int j=i+1;j<nums.length;j++){
+//                int third = -(nums[i]+nums[j]);
+//                if(hashSet.contains(third)){
+//                    List<Integer>list = Arrays.asList(nums[i],nums[j],third);
+//                    Collections.sort(list);
+//                    set.add(list);
+//                }
+//                hashSet.add(nums[j]);
+//            }
+//        }
+//        return new ArrayList<>(set);
+//    }
+
+//    Method-3  T.C=O(n^2) || S.C=O(k)  and O(n^2) in worst case
     public static List<List<Integer>> threeSum(int[] nums){
-        Set<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
         for(int i=0;i<nums.length;i++){
-            Set<Integer>hashSet = new HashSet<>();
-            for(int j=i+1;j<nums.length;j++){
-                int third = -(nums[i]+nums[j]);
-                if(hashSet.contains(third)){
-                    List<Integer>list = Arrays.asList(nums[i],nums[j],third);
-                    Collections.sort(list);
-                    set.add(list);
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
+                }else if(sum>0){
+                    k--;
+                }else{
+                    List<Integer>ls = Arrays.asList(nums[i],nums[j],nums[k]);
+                    list.add(ls);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
                 }
-                hashSet.add(nums[j]);
             }
         }
-        return new ArrayList<>(set);
+        return list;
     }
 }
