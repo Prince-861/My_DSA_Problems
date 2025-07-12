6280f52f280c88a36921ff9e0f894db5d0aeaaa0
Prince-861 <pk868317@example.com>
2025-07-12 23:50:01 +0530

    lengthOfLastWord using Stream Added

---
+++ src/com/arrays/LengthOfLastWord.java
@@ -0,0 +1,17 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+        String s ="   fly me   to   the moon  ";
+        System.out.println(lengthOfLastWord(s));
+    }
+//    Method-1
+    public static int lengthOfLastWord(String s){
+        return Arrays.stream(s.trim().split("\\s+"))
+                .reduce((first,second)->second)
+                .orElse("")
+                .length();
+    }
+}
