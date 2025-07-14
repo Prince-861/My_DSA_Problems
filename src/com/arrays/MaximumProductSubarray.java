472626f0ffca137bf7f528aad1ef63333057d26b
Prince-861 <pk868317@example.com>
2025-07-14 23:59:12 +0530

    MaximumProductSubArray brute force approach added

---
+++ src/com/arrays/MaximumProductSubarray.java
@@ -0,0 +1,19 @@
+package com.arrays;
+
+public class MaximumProductSubarray {
+    public static void main(String[] args) {
+        int nums[] = {-2,0,-1};
+        System.out.println(maxProduct(nums));
+    }
+    public static int maxProduct(int[] nums){
+        int maxProduct = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            int prod=1;
+            for(int j=i;j<nums.length;j++){
+                prod*=nums[j];
+                maxProduct = Math.max(maxProduct,prod);
+            }
+        }
+        return maxProduct;
+    }
+}
