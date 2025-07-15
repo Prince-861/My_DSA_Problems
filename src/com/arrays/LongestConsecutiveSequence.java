af827ae1eec031245673d7bb985cea1e56752c28
Prince-861 <pk868317@gmail.com>
2025-07-16 00:32:11 +0530

    Longest Consecutive Sequence using Sorting approach added

--- src/com/arrays/LongestConsecutiveSequence.java
+++ src/com/arrays/LongestConsecutiveSequence.java
@@ -1,5 +1,7 @@
 package com.arrays;
 
+import java.util.Arrays;
+
 //Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence
 //Example 1:
 //Input: nums = [100,4,200,1,3,2]
@@ -18,24 +20,42 @@ public class LongestConsecutiveSequence {
         int ans = longestConsecutive(nums);
         System.out.println(ans);
     }
+//    Brute force approach - O(n^2)
+//    public static int longestConsecutive(int[] nums){
+//        int n = nums.length;
+//        int maxCount = 0;
+//        for(int i=0;i<n;i++){
+//            int x=nums[i];
+//            int count=1;
+//            while(linearSearch(nums,x+1)==true){
+//                count++;
+//                x=x+1;
+//            }
+//            maxCount = Math.max(maxCount,count);
+//        }
+//        return maxCount;
+//    }
+//    public static boolean linearSearch(int nums[], int x){
+//        for(int i=0;i<nums.length;i++){
+//            if(nums[i]==x) return true;
+//        }
+//        return false;
+//    }
+//    Method-2 Using Sorting T.C=O(nlogn)
     public static int longestConsecutive(int[] nums){
-        int n = nums.length;
-        int maxCount = 0;
-        for(int i=0;i<n;i++){
-            int x=nums[i];
-            int count=1;
-            while(linearSearch(nums,x+1)==true){
+        if(nums==null || nums.length==0) return 0;
+        Arrays.sort(nums);
+        int count=1;
+        int maxCount=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]) continue;
+            if(nums[i]==nums[i-1]+1){
                 count++;
-                x=x+1;
+                maxCount = Math.max(maxCount,count);
+            }else{
+                count=1;
             }
-            maxCount = Math.max(maxCount,count);
         }
         return maxCount;
-    }
-    public static boolean linearSearch(int nums[], int x){
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==x) return true;
-        }
-        return false;
     }
 }
