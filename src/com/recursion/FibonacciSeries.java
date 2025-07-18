f9a2ad503a306f00897b4894e5eebbcdd16e5d95
Prince-861 <pk868317@gmail.com>
2025-07-18 22:15:16 +0530

    Fibonacci Series code added

---
+++ src/com/recursion/FibonacciSeries.java
@@ -0,0 +1,19 @@
+package com.recursion;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n=10;
+        fibSeries(n);
+    }
+    public static void fibSeries(int n){
+        int n1=0;
+        int n2=1;
+        System.out.print(n1+" "+n2+" ");
+        for(int i=2;i<=n;i++){
+            int n3=n1+n2;
+            System.out.print(n3+" ");
+            n1=n2;
+            n2=n3;
+        }
+    }
+}
