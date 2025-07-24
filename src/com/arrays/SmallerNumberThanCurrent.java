5a56c3e66a07bc15c8c0f9489d2d5b222b4cb805
Prince-861 <pk868317@gmail.com>
2025-07-24 23:55:53 +0530

    smaller no. than current in array optimized approach

--- src/com/arrays/SmallerNumberThanCurrent.java
+++ src/com/arrays/SmallerNumberThanCurrent.java
@@ -19,22 +19,49 @@ import java.util.Arrays;
 //Example 3:
 //Input: nums = [7,7,7,7]
 //Output: [0,0,0,0]
+
+//Constraints:
+//2 <= nums.length <= 500
+//0 <= nums[i] <= 100
 public class SmallerNumberThanCurrent {
     public static void main(String[] args) {
-        int nums[] = {7,7,7,7};
+        int nums[] = {6,5,3,1};
         int ans[] = smallerNumber(nums);
         System.out.println(Arrays.toString(ans));
     }
-    public static int[] smallerNumber(int[] nums){
-        int n = nums.length;
-        int ans[] = new int[nums.length];
-        for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(nums[j]<nums[i]) count++;
-            }
-            ans[i]=count;
+//    Method-1
+//    public static int[] smallerNumber(int[] nums){
+//        int n = nums.length;
+//        int ans[] = new int[nums.length];
+//        for(int i=0;i<n;i++){
+//            int count=0;
+//            for(int j=0;j<n;j++){
+//                if(nums[j]<nums[i]) count++;
+//            }
+//            ans[i]=count;
+//        }
+//        return ans;
+//    }
+
+//    Method-2
+    public static int[] smallerNumber(int nums[]){
+        int[] ans = new int[101];
+        for(int i=0;i<nums.length;i++){
+            ans[nums[i]]++;
         }
-        return ans;
+        System.out.println(Arrays.toString(ans));
+
+        for(int i=1;i<ans.length;i++){
+            ans[i]+=ans[i-1];
+        }
+
+        System.out.println(Arrays.toString(ans));
+
+        int[] result = new int[nums.length];
+        for(int i=0;i<result.length;i++){
+            if(nums[i]==0) result[i]=0;
+            else result[i]=ans[nums[i]-1];
+        }
+        return result;
     }
 }
