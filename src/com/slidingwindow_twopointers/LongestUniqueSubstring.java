0d1f923ec6310e10515051bfbb1494e4c7582d29
Prince-861 <pk868317@gmail.com>
2025-08-02 00:57:25 +0530

    LongestUniqueSubstring optimized method using HashSet added

--- src/com/slidingwindow_twopointers/LongestUniqueSubstring.java
+++ src/com/slidingwindow_twopointers/LongestUniqueSubstring.java
@@ -16,24 +16,41 @@ import java.util.Set;
 //Explanation: The answer is "b", with the length of 1.
 public class LongestUniqueSubstring {
     public static void main(String[] args) {
-        String s = "abcabcbb";
+//        String s = "abcabcbb";//3
+        String s = "bbbbb";//1
         System.out.println(lengthOfLongestSubstring(s));
     }
-    public static int lengthOfLongestSubstring(String s) {
+//    Method-1(T.C=O(n^2))
+//    public static int lengthOfLongestSubstring(String s) {
+//        int maxLength = 0;
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            Set<Character> seen = new HashSet<>();
+//            for (int j = i; j < s.length(); j++) {
+//                char c = s.charAt(j);
+//                if (seen.contains(c)) {
+//                    break;
+//                }
+//                seen.add(c);
+//                maxLength = Math.max(maxLength, j - i + 1);
+//            }
+//        }
+//
+//        return maxLength;
+//    }
+
+    public static int lengthOfLongestSubstring(String s){
+        Set<Character> set = new HashSet<>();
+        int n = s.length();
         int maxLength = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            Set<Character> seen = new HashSet<>();
-            for (int j = i; j < s.length(); j++) {
-                char c = s.charAt(j);
-                if (seen.contains(c)) {
-                    break;
-                }
-                seen.add(c);
-                maxLength = Math.max(maxLength, j - i + 1);
+        int l=0;
+        for(int r=0;r<n;r++){
+            while(r<n && set.contains(s.charAt(r))){
+                set.remove(s.charAt(l++));
             }
+            set.add(s.charAt(r));
+            maxLength = Math.max(r-l+1,maxLength);
         }
-
-        return maxLength;
+        return  maxLength;
     }
 }
