bcde13841841a07d0ae974c0f0705d7448f332a5
Prince-861 <pk868317@gmail.com>
2025-08-12 18:02:56 +0530

    DeleteTailNodeInLL modified code added

--- src/com/linkedlist/DeleteTailNodeInLL.java
+++ src/com/linkedlist/DeleteTailNodeInLL.java
@@ -9,11 +9,12 @@ public class DeleteTailNodeInLL {
         head.next.next = new Node(3);
         head.next.next.next = new Node(4);
         printLL(head);
-        deleteTailNode(head);
+        head = deleteTailNode(head);
         printLL(head);
     }
 //    Method-1 (Using prev pointer)
-//    public static void deleteTailNode(Node head){
+//    public static Node deleteTailNode(Node head){
+//        if(head==null || head.next==null) return null;
 //        Node curr = head;
 //        Node prev = null;
 //        while(curr.next!=null){
@@ -21,14 +22,17 @@ public class DeleteTailNodeInLL {
 //            curr = curr.next;
 //        }
 //        prev.next = null;
+//        return head;
 //    }
 
 //    Method-2
-    public static void deleteTailNode(Node head){
+    public static Node deleteTailNode(Node head){
+        if(head==null || head.next==null) return null;
         Node curr = head;
         while(curr.next.next!=null){
             curr = curr.next;
         }
         curr.next = null;
+        return head;
     }
 }
