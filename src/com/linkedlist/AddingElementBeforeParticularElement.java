28edf997d72c21824c73b7fe9786cb411d830bda
Prince-861 <pk868317@gmail.com>
2025-08-12 10:34:35 +0530

    Adding Element before a particular element using prev Node in the LL

--- src/com/linkedlist/AddingElementBeforeParticularElement.java
+++ src/com/linkedlist/AddingElementBeforeParticularElement.java
@@ -8,21 +8,44 @@ public class AddingElementBeforeParticularElement {
         head.next.next.next = new Node2('d');
         addElementBeforeParticularElement(head);
     }
+//    method-1 ==> checking temp.next.data!='b'
     //Add 'p' before 'b'
-    public static void addElementBeforeParticularElement(Node2 head){
+//    public static void addElementBeforeParticularElement(Node2 head){
+//        Node2 newNode = new Node2('p');
+//        Node2 temp = head;
+//        while(temp.next.data!='b'){
+//            temp=temp.next;
+//        }
+//        newNode.next = temp.next;
+//        temp.next = newNode;
+//
+//        //printing the LL
+//        Node2 curr = head;
+//        while(curr!=null){
+//            System.out.print(curr.data+" -> ");
+//            curr = curr.next;
+//        }
+//        System.out.println("null");
+//    }
+
+    //    Method-2 (We will maintain one previous Node)
+    //Adding 'p' before 'd'
+    public static void addElementBeforeParticularElement(Node2 head) {
         Node2 newNode = new Node2('p');
         Node2 temp = head;
-        while(temp.next.data!='b'){
+        Node2 prev = null;
+        while(temp.data!='d'){
+            prev = temp;
             temp=temp.next;
         }
-        newNode.next = temp.next;
-        temp.next = newNode;
+        newNode.next = temp;
+        prev.next = newNode;
 
-        //printing the LL
+        //print the LL
         Node2 curr = head;
         while(curr!=null){
-            System.out.print(curr.data+" -> ");
-            curr = curr.next;
+            System.out.print(curr.data + " -> ");
+            curr=curr.next;
         }
         System.out.println("null");
     }
