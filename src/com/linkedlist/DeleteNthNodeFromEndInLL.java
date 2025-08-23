14751420ac6f5188ea0d9a89366d98cdcb27f058
Prince-861 <pk868317@gmail.com>
2025-08-23 23:07:07 +0530

    DeleteNthNodeFromEndInLL optimized code added

--- src/com/linkedlist/DeleteNthNodeFromEndInLL.java
+++ src/com/linkedlist/DeleteNthNodeFromEndInLL.java
@@ -17,26 +17,44 @@ public class DeleteNthNodeFromEndInLL {
         head = deleteNthNodeFromEnd(head,n);
         printList(head);
     }
-    public static ListNode deleteNthNodeFromEnd(ListNode head, int n){
-        if(head==null) return null;
-        int count = 0;
-        ListNode temp = head;
-        while(temp!=null){
-            count++;
-            temp = temp.next;
+
+    //Method-1
+//    public static ListNode deleteNthNodeFromEnd(ListNode head, int n){
+//        if(head==null) return null;
+//        int count = 0;
+//        ListNode temp = head;
+//        while(temp!=null){
+//            count++;
+//            temp = temp.next;
+//        }
+//        if(count==n){
+//            return head.next;
+//        }
+//        int res = count-n;//we have to traverse till res Node and in this (temp)node's next we have to point to the next.next node
+//        temp = head;
+//        while(temp!=null){
+//            res--;
+//            if(res==0) break;
+//            temp = temp.next;
+//        }
+//        ListNode deleteNode = temp.next;
+//        temp.next = deleteNode.next;
+//        return head;
+//    }
+
+    //Method-2
+    public static ListNode deleteNthNodeFromEnd(ListNode head,int n){
+        if(head == null) return head;
+        ListNode fast = head;
+        ListNode slow = head;
+        for(int i=0;i<n;i++) fast = fast.next;
+
+        if(fast == null) return head.next;
+        while(fast.next!=null){
+            slow = slow.next;
+            fast = fast.next;
         }
-        if(count==n){
-            return head.next;
-        }
-        int res = count-n;//we have to traverse till res Node and in this (temp)node's next we have to point to the next.next node
-        temp = head;
-        while(temp!=null){
-            res--;
-            if(res==0) break;
-            temp = temp.next;
-        }
-        ListNode deleteNode = temp.next;
-        temp.next = deleteNode.next;
+        slow.next = slow.next.next;
         return head;
     }
 }
