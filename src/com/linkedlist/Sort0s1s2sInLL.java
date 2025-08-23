09ce66b42e01452ac1283dcf779ae95fd5fe73af
Prince-861 <pk868317@gmail.com>
2025-08-23 15:03:06 +0530

    Sort0s1s2sInLL code added using dummy nodes

--- src/com/linkedlist/Sort0s1s2sInLL.java
+++ src/com/linkedlist/Sort0s1s2sInLL.java
@@ -22,33 +22,68 @@ public class Sort0s1s2sInLL {
         printList(head);
     }
 
-    public static ListNode sortList(ListNode head){
-        if(head==null || head.next==null) return head;
+//    Method-1
+//    public static ListNode sortList(ListNode head){
+//        if(head==null || head.next==null) return head;
+//
+//        int zero=0,one=0,two=0;
+//        ListNode temp = head;
+//        while(temp!=null){
+//            int data=temp.val;
+//            if(data==0) zero++;
+//            else if(data==1) one++;
+//            else two++;
+//
+//            temp = temp.next;
+//        }
+//        temp = head;
+//        while(temp!=null){
+//            if(zero!=0){
+//                temp.val=0;
+//                zero--;
+//            }else if(one!=0){
+//                temp.val=1;
+//                one--;
+//            }else{
+//                temp.val=2;
+//                two--;
+//            }
+//            temp = temp.next;
+//        }
+//        return head;
+//    }
 
-        int zero=0,one=0,two=0;
+//    Method-2
+    public static ListNode sortList(ListNode head){
+        if(head == null || head.next == null) return head;
+
+        ListNode zeroHead = new ListNode(-1);
+        ListNode zero = zeroHead;
+
+        ListNode oneHead = new ListNode(-1);
+        ListNode one = oneHead;
+
+        ListNode twoHead = new ListNode(-1);
+        ListNode two = twoHead;
+
         ListNode temp = head;
         while(temp!=null){
-            int data=temp.val;
-            if(data==0) zero++;
-            else if(data==1) one++;
-            else two++;
-
-            temp = temp.next;
-        }
-        temp = head;
-        while(temp!=null){
-            if(zero!=0){
-                temp.val=0;
-                zero--;
-            }else if(one!=0){
-                temp.val=1;
-                one--;
+            if(temp.val==0){
+                zero.next = temp;
+                zero = temp;
+            }else if(temp.val==1){
+                one.next = temp;
+                one = temp;
             }else{
-                temp.val=2;
-                two--;
+                two.next = temp;
+                two = temp;
             }
             temp = temp.next;
         }
-        return head;
+        zero.next = oneHead.next!=null ? oneHead.next : twoHead.next;
+        one.next = twoHead.next;
+        two.next = null;
+
+        return zeroHead.next;
     }
 }
