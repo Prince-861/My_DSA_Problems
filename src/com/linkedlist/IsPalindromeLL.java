918d9aa664510467e4d5306cda657ae997b5271c
Prince-861 <pk868317@gmail.com>
2025-08-24 14:57:42 +0530

    Palindrome LL code added using deep copy

---
+++ src/com/linkedlist/IsPalindromeLL.java
@@ -0,0 +1,53 @@
+package com.linkedlist;
+
+import static com.linkedlist.AddTwoNoInLL.printList;
+
+public class IsPalindromeLL {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
+
+        printList(head);
+        boolean val = isPalindromeLL(head);
+        System.out.println(val);
+    }
+
+    public static boolean isPalindromeLL(ListNode head){
+        if(head==null || head.next == null) return true;
+
+        ListNode temp = head;
+        ListNode copyHead = null, copyTail = null;
+        while(temp!=null){
+            ListNode newNode = new ListNode(temp.val);//Here we are creating new node again and again and doing deep copy
+            if(copyHead==null){
+                copyHead = newNode;
+            }else{
+                copyTail.next = newNode;
+            }
+            copyTail = newNode;
+            temp = temp.next;
+        }
+
+        ListNode revHead = reverseList(copyHead);
+        temp = head;
+        while(temp!=null){
+            if(temp.val!=revHead.val) return false;
+            temp = temp.next;
+            revHead = revHead.next;
+        }
+        return true;
+    }
+    public static ListNode reverseList(ListNode head){
+        ListNode temp = head;
+        ListNode prev = null;
+        while(temp!=null){
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+}
