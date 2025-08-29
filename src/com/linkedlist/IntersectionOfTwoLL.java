26dab5307d291f9eef4d152c5c0774b7c774d571
Prince-861 <pk868317@gmail.com>
2025-08-29 23:57:32 +0530

    IntersectionOfTwoLL code added

---
+++ src/com/linkedlist/IntersectionOfTwoLL.java
@@ -0,0 +1,50 @@
+package com.linkedlist;
+
+import java.util.HashSet;
+
+public class IntersectionOfTwoLL {
+    public static void main(String[] args) {
+        ListNode headA = new ListNode(4);
+        headA.next = new ListNode(1);
+        ListNode intersect = new ListNode(8);
+        headA.next.next = intersect;
+        intersect.next = new ListNode(4);
+        intersect.next.next = new ListNode(5);
+
+        // Create List B: 5 -> 6 -> 1 -> 8 -> 4 -> 5
+        ListNode headB = new ListNode(5);
+        headB.next = new ListNode(6);
+        headB.next.next = new ListNode(1);
+        headB.next.next.next = intersect; // Linking intersection point
+
+        // Find intersection
+        ListNode result = getIntersectionNode(headA, headB);
+
+        if (result != null) {
+            System.out.println("Intersected at '" + result.val + "'");
+        } else {
+            System.out.println("No intersection");
+        }
+    }
+
+    // Method to find intersection using HashSet
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode> visited = new HashSet<>();
+
+        // Store nodes of list A
+        while (headA != null) {
+            visited.add(headA);
+            headA = headA.next;
+        }
+
+        // Check nodes of list B
+        while (headB != null) {
+            if (visited.contains(headB)) {
+                return headB; // Intersection found
+            }
+            headB = headB.next;
+        }
+
+        return null; // No intersection
+    }
+}
