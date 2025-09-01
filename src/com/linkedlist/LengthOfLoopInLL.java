c1b344add933edb26ba3c67a4db6029bd440efab
Prince-861 <pk868317@gmail.com>
2025-09-01 23:36:26 +0530

    LengthOfLoopInLL using HashMap code added

---
+++ src/com/linkedlist/LengthOfLoopInLL.java
@@ -0,0 +1,35 @@
+package com.linkedlist;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class LengthOfLoopInLL {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+        head.next.next.next.next.next.next.next = new ListNode(8);
+        head.next.next.next.next.next.next.next.next = new ListNode(9);
+        head.next.next.next.next.next.next.next.next.next = head.next.next;
+
+        System.out.println(lengthOfLoop(head));
+    }
+    public static int lengthOfLoop(ListNode head){
+        if(head==null) return -1;
+        ListNode temp = head;
+        HashMap<ListNode,Integer> map = new HashMap<>();
+        int timer=1;
+        while(temp!=null){
+            if(map.containsKey(temp)){
+                return timer-map.get(temp);
+            }
+            map.put(temp,timer++);
+            temp = temp.next;
+        }
+        return -1;
+    }
+}
